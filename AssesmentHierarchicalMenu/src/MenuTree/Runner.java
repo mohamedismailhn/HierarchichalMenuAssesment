a6ce45ef7234c7c024c1f08fba68d7958b6701e4
mohamedismailhn <mohamedismailhn@localhost>
2022-09-30 20:14:42 +0530

    Variable changes

--- AssesmentHierarchicalMenu/src/MenuTree/Runner.java
+++ AssesmentHierarchicalMenu/src/MenuTree/Runner.java
@@ -7,11 +7,11 @@ public class Runner {
 	public static void main(String[] ar) {
 		
 		JFrame jFrame= new JFrame("Assesment-Menu Hierarchy");
-        JMenuBar m=new JMenuBar();
+        JMenuBar menuBar=new JMenuBar();
         
-		MenuFunctionality.displayMenu(m);
+		MenuFunctionality.displayMenu(menuBar);
 		
-		jFrame.setJMenuBar(m);
+		jFrame.setJMenuBar(menuBar);
         jFrame.setVisible(true);
         jFrame.setExtendedState(jFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 	}
